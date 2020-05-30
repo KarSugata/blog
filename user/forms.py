38324c4ba743fd6e3558bfff1dc99f354e07840b
KarSugata <sugatak99@outlook.com>
2020-05-30 18:49:45 +0530

    commit

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=200)
     class meta:
--- user/views.py
+++ user/views.py
@@ -9,15 +9,11 @@ def SignUpView(request):
         form = SignUpForm(request.POST)
         
         if form.is_valid():
-            # user = form.save()
-            # user.refresh_from_db()
-            # user.profile.first_name = form.cleaned_data.get('first_name')
-            # user.profile.last_name = form.cleaned_data.get('last_name')
-            # user.profile.email = form.cleaned_data.get('email')
-            # user.save()
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            print(f'Password: {password} and Username: {username} and email: {email}')
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('blog-home')
