9c4d60287892b2215985ec7e48d302146969628a
KarSugata <sugatak99@outlook.com>
2020-05-30 10:31:45 +0530

    commit

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
-    
+    email = forms.EmailField(max_length=200)
+    class meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -8,7 +9,13 @@ class Profile(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
     bio = models.CharField(blank=True, max_length=500)
     location = models.CharField(blank=True, max_length=50)
+    email = models.EmailField(unique=True, max_length=200)
 
     def __str__(self):
         return f'Profile for {self.user.username}'
-    
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
