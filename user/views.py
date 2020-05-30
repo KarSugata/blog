4ab1dd96769400b7672de7297633a0ce5ef2e071
KarSugata <sugatak99@outlook.com>
2020-05-30 10:38:10 +0530

    commit

--- user/views.py
+++ user/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 # Create your views here.
 
 def SignUpView(request):
-    # if request.method == 'POST':
+    
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.first_name = form.cleaned_data.get('first_name')
+            user.profile.last_name = form.cleaned_data.get('last_name')
+            user.profile.email = form.cleaned_data.get('email')
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('blog-home')
+    else:
+        
+        form = SignUpForm()
 
-    context = {}
+    context = {'form':form}
+    
     return render(request, 'user/signup.html', context=context)
 
 def loginView(request):
