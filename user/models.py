c944ab81e94e6efc7e03bda6b40c873d2690636f
KarSugata <sugatak99@outlook.com>
2020-07-13 10:38:53 +0530

    commit

--- user/models.py
+++ user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
 # Create your models here.
 
 class Profile(models.Model):
